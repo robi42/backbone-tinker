45441ce93933c5179af9af89053b6ad28ad27a70
Robert Thurnher <r.thurnher@gmail.com>
2011-04-10 00:25:38 +0200

    Code syle refinements: using implicit infix notation.

--- backend/project/build/Project.scala
+++ backend/project/build/Project.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
-  val sonatypeRepo    = MavenRepository("Sonatype Releases", "http://oss.sonatype.org/content/repositories/releases")
-  val scalaSnapshots  = MavenRepository("Scala-tools.org Repo", "http://scala-tools.org/repo-snapshots/")
-  val scalableRepo    = MavenRepository("Akka Repo",            "http://scalablesolutions.se/akka/repository")
-  val jbossReleases   = MavenRepository("JBoss Releases", "http://repository.jboss.org/nexus/content/groups/public/")
+  val sonatypeRepo    = "Sonatype Releases"    at "http://oss.sonatype.org/content/repositories/releases"
+  val scalaSnapshots  = "Scala-tools.org Repo" at "http://scala-tools.org/repo-snapshots/"
+  val scalableRepo    = "Akka Repo"            at "http://scalablesolutions.se/akka/repository"
+  val jbossReleases   = "JBoss Releases"       at "http://repository.jboss.org/nexus/content/groups/public/"
 
   val blueeyesRelease = "com.github.blueeyes" % "blueeyes" % "0.2.7" % "compile"
 }
